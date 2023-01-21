a6e9bec3d11c3925c79bcada2cb7621001d7f8ad
shalin-1000 <122726739+shalin-1000@users.noreply.github.com>
2023-01-21 22:14:03 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 echo "Creating directory /tmp/test_1"
 mkdir -p /tmp/test_1/
 
-echo "Copying file /var/lib/jenkins/test.txt to /tmp/scmpoll.txt"
+echo "Copying file /var/lib/jenkins/test.txt to /tmp/scmpoll_1.txt"
 cp /var/lib/jenkins/test.txt /tmp/scmpoll.txt
 
 echo "File copy completed"
