62bb8f105e119b3c53cb456ade58e127fb7bbad1
shalin-1000 <122726739+shalin-1000@users.noreply.github.com>
2023-01-21 20:05:08 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating directory /tmp/test_1"
-mkdir -p /tmp/test
+mkdir -p /tmp/test/
 
 echo "Copying file /var/lib/jenkins/workspace/shell_main/test.sh to /tmp/test"
 cp /var/lib/jenkins/workspace/shell_main/test.sh /tmp/test/
