c7a5a4f5f1ffdef73481122288954b749094ba29
shalin-1000 <122726739+shalin-1000@users.noreply.github.com>
2023-01-21 22:55:14 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-echo "Creating directory /tmp/test_1"
-mkdir -p /tmp/test_1/
+echo "Installing Docker"
 
-echo "Copying file /var/lib/jenkins/test.txt to /tmp/scmpoll_1.txt"
-cp /var/lib/jenkins/test.txt /tmp/scmpoll.txt
+# Update the package lists
+sudo apt-get update
 
-echo "File copy completed"
+# Install package dependencies
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+# Add the Docker repository key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Verify the fingerprint of the key
+sudo apt-key fingerprint 0EBFCD88
+
+# Add the Docker repository to the system
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update the package lists again
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Start the Docker service
+sudo systemctl start docker
+
+# Enable the Docker service to start on boot
+sudo systemctl enable docker
+
+# Verify the installation
+docker --version
+
+echo "Docker installed successfully"
