f164221325f4ecc0c298ac35cce257b640f2b4a2
shalin-1000 <122726739+shalin-1000@users.noreply.github.com>
2023-01-21 19:35:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-echo "Uninstalling Terraform"
+echo "Installing Docker"
 
-# Check if Terraform is installed
-if [ -f /usr/local/bin/terraform ]; then
-    echo "Terraform is installed"
-else
-    echo "Terraform is not installed"
-    exit 1
-fi
+# Update the package lists
+sudo apt-get update
 
-# Remove the Terraform binary
-sudo rm /usr/local/bin/terraform
+# Install package dependencies
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-# Remove Terraform configuration files
-sudo rm -rf ~/.terraform.d
+# Add the Docker repository key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo "Terraform uninstalled successfully"
+# Verify the fingerprint of the key
+sudo apt-key fingerprint 0EBFCD88
+
+# Add the Docker repository to the system
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update the package lists again
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Start the Docker service
+sudo systemctl start docker
+
+# Enable the Docker service to start on boot
+sudo systemctl enable docker
+
+# Verify the installation
+docker --version
+
+echo "Docker installed successfully"
