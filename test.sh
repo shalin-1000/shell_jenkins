231d029256728515428b63b49c3932a771a60863
shalin-1000 <122726739+shalin-1000@users.noreply.github.com>
2023-01-23 07:28:45 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ jenkins@ip-10-0-1-104:/tmp$ cat jen.sh
 #!/bin/bash
 
 echo "Creating directory /tmp/test_1"
-mkdir -p /var/lib/jenkins/test/
-cd /var/lib/jenkins/test
+mkdir -p /tmp/test/
+cd /tmp/test/
 touch test.txt
-mkdir -p /var/lib/jenkins/test_2/ 
+mkdir -p /tmp/test/webhook/
 
 echo "Copying file /var/lib/jenkins/workspace/shell_main/test.sh to /tmp/test"
-cp /var/lib/jenkins/test/test.txt /var/lib/jenkins/test_1/jen_webhook.txt
+cp /tmp/test/* /tmp/test/webhook/webhook.txt
